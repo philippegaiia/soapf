37739a7cf3e73a99b3dd33f44b11884cfd87ca89
Phlippe Maradan <pmaradan@gmail.com>
2020-12-11 09:00:57 +0100

    small action buttons added

---
+++ resources/views/components/buttons/edit-button.blade.php
@@ -0,0 +1,3 @@
+<button {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex items-center justify-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-red active:bg-bue-600 transition ease-in-out duration-150']) }}>
+    {{ $slot }}
+</button>
--- resources/views/suppliers/index.blade.php
+++ resources/views/suppliers/index.blade.php
@@ -9,14 +9,13 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class=" mx-auto p-6 bg-white border-b border-gray-200">
-                <div class="inline-flex align-center mb-2 ">
-
-        <a href="{{ route('suppliers.create') }}" >
-                                <x-secondary-button class="block">
-                                    {{ __('Créer un nouveau fournisseur') }}
-                                </x-secondary-button>
-                            </a>
-
+                {{-- <div class="inline-flex align-center mb-2 "> --}}
+                <div class="flex items-center  py-3 text-right  sm:justify-center md:justify-end ">
+                    <a href="{{ route('suppliers.create') }}" >
+                        <x-buttons.secondary-button class="block">
+                            {{ __('Créer un nouveau fournisseur') }}
+                        </x-buttons.secondary-button>
+                    </a>
                 </div>
                 <div class="">
                     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -25,22 +24,22 @@
                                 <table class="min-w-full divide-y divide-gray-200">
                                     <thead class="bg-gray-50">
                                         <tr>
-                                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            <th scope="col" class="px-6 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                 Code
                                             </th>
-                                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            <th scope="col" class="px-6 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                 Société
                                             </th>
-                                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            <th scope="col" class="px-6 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                 Statut
                                             </th>
-                                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            <th scope="col" class="px-6 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                 Ville
                                             </th>
-                                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            <th scope="col" class="px-6 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                 Actions
                                             </th>
-                                            <th scope="col" class="relative px-6 py-3">
+                                            <th scope="col" class="relative px-6 py-2">
                                                 <span class="sr-only">Edit</span>
                                             </th>
                                         </tr>
@@ -48,7 +47,7 @@
                                     <tbody class="bg-white divide-y divide-gray-200">
                                         @forelse ($suppliers as $supplier)
                                             <tr>
-                                                <td class="px-3 py-4 whitespace-nowrap">
+                                                <td class="px-3 py-2 whitespace-nowrap">
                                                     <div class="flex items-center">
                                                         <div class="ml-4">
                                                             <div class="text-sm font-medium text-gray-900">
@@ -57,7 +56,8 @@
                                                         </div>
                                                     </div>
                                                 </td>
-                                                <td class="px-3 py-4 whitespace-nowrap">
+
+                                                <td class="px-3 py-2 whitespace-nowrap">
                                                     <div class="flex items-center">
                                                         <div class="ml-4">
                                                             <div class="text-sm font-medium text-gray-900">
@@ -67,23 +67,34 @@
                                                     </div>
                                                 </td>
 
-                                                <td class="px-6 py-4 whitespace-nowrap">
+                                                <td class="px-6 py-2 whitespace-nowrap">
                                                     <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full {{ $supplier->active == 'Actif' ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800' }}">
                                                         {{ $supplier->active }}
                                                     </span>
                                                 </td>
-                                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+
+                                                <td class="px-6 py-2 whitespace-nowrap text-sm text-gray-500">
                                                     {{ $supplier->zip }} {{ $supplier->city }}
                                                 </td>
 
-                                                <td class="px-3 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                <td class=" whitespace-nowrap py-2 flex">
                                                         <a href="{{ route('suppliers.create') }}">
-                                                    <x-button class="ml-4">
-                                                        {{ __('Créer') }}
-                                                    </x-button>
-                                                </a>
+                                                            <x-buttons.show-button-sm class="ml-4">
 
-                                                <x-jetbutton>Jetbutton</x-jetbutton>
+                                                            </x-buttons.show-button-sm>
+                                                        </a>
+
+                                                        <a href="{{ route('suppliers.create') }}">
+                                                            <x-buttons.edit-button-sm class="ml-4">
+
+                                                            </x-buttons.edit-button-sm>
+                                                        </a>
+
+                                                         <a href="{{ route('suppliers.create') }}">
+                                                            <x-buttons.delete-button-sm class="ml-4">
+                                                                {{ __('') }}
+                                                            </x-buttons.delete-button-sm>
+                                                        </a>
                                                 </td>
 
 
